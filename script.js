0e44c0cce24c4fa693a568d97254057d72ae9a9f
Wendy Cruz <wc1769338@gmail.com>
2025-03-10 02:33:24 -0400

    segundo commit

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const file = poster.src
 const blur = document.querySelector("#blur")
 blur.style.backgroundImage = `url(${file})`
 console.log(blur.style)
+
+const flecha = document.getElementById("flecha")
+const menu = document.getElementById("mainMenu")
+flecha.addEventListener("click", () => {
+ menu.classList.toggle("hide")//toggle si encuentra la clase la quito si no la agrego
+})
